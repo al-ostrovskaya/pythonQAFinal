7079d2950f587e75d3dfdc98c233e4e4df5398a3
schiwa13 <42767613+schiwa13@users.noreply.github.com>
2024-02-24 09:54:10 +0300

    Подготовка тестового окружения нового проекта

---
+++ conftest.py
@@ -0,0 +1,46 @@
+import pytest
+
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_name',
+        action='store',
+        default='Chrome',  # или default=None,
+        help='Choose browser: Chrome, Firefox, Edge'
+    )
+    parser.addoption(
+        '--language',
+        action='store',
+        default='ru, en',  # или default=None,
+        help='Language'
+    )
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    browser_name = request.config.getoption('browser_name').capitalize()
+    language = request.config.getoption('language')
+    print(f'\nStart browser {browser_name} for test..')
+    if browser_name == 'Firefox':
+        options = webdriver.FirefoxProfile()
+        options.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=options)
+    elif browser_name == 'Chrome':
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        if browser_name != 'Chrome':
+            print(f'\nDriver {browser_name} not found!')
+            print('\nStart default browser Chrome for test..')
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    # driver.implicitly_wait(5)
+
+    yield browser
+
+    print('\nQuit browser..')
+    browser.quit()
